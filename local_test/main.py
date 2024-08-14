7352ceaa8900b08afac10ddcdf539f099ed09964
emanzek <muhdaimanzaidi98@gmail.com>
2024-08-14 22:08:07 +0800

    Completing SES implementation for token

--- local_test/main.py
+++ local_test/main.py
@@ -1,10 +1,24 @@
 import requests
 import json
+import datetime
 
 url = "http://localhost:3000/common"
+filepath = './event.json'
 
-with open('./event.json','r') as file:
-    payload = json.load(file)
+def main():
+    payload = read_json(filepath)    
     response = requests.request("POST", url, data=json.dumps(payload))
+    print(response.text)
 
-print(response.text)
+def read_json(data):
+    with open(data,'r') as file:
+        data = json.load(file)
+        
+        # Update timestamp to current time
+        time_now = int(datetime.datetime.now().timestamp())
+        data['message']['date'] = time_now
+        
+    return data
+
+if __name__ == "__main__":
+    main()
